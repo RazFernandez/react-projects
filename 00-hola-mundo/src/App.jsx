541d84e694ed4556a440ef701fc333058f5fec1d
RazFernandez <mr.fernandez.mx@outlook.com>
2025-04-10 00:39:47 -0500

    🎨 style: Added style.scss for compiling css files using sass

---
+++ 00-hola-mundo/src/App.jsx
@@ -0,0 +1,19 @@
+export function App() {
+    return (
+        <article>
+            <header>
+                <img alt="Avatar Miguel Fernandez" src="https://avatars.githubusercontent.com/u/96704399?v=4"></img>
+                <div>
+                    <strong>Miguel Fernandez</strong>
+                    <span>@miguelfernandez</span>
+                </div>
+            </header>
+
+            <aside>
+                <button>
+                    Seguir
+                </button>
+            </aside>
+        </article>
+    )
+}
